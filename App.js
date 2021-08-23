7afa2f6619a534d5c3e1c66e50111aba94b72c1b
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-08-23 15:37:09 +0300

    ekatunti

--- App.js
+++ App.js
@@ -1,72 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {
-  FlatList,
-  Image,
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import List from './components/List';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.droidSafeArea}>
       <View style={styles.container}>
-        <FlatList
-          data={mediaArray}
-          renderItem={({item}) => {
-            return (
-              <TouchableOpacity style={styles.row}>
-                <View style={styles.imagebox}>
-                  <Image
-                    style={styles.image}
-                    source={{uri: item.thumbnails.w160}}
-                  />
-                </View>
-                <View style={styles.textbox}>
-                  <Text style={styles.listTitle}>{item.title}</Text>
-                  <Text>{item.description}</Text>
-                </View>
-              </TouchableOpacity>
-            );
-          }}
-        />
+        <List />
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -81,30 +22,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ccc',
     paddingTop: Platform.OS === 'android' ? 25 : 0,
-  },
-  row: {
-    flexDirection: 'row',
-    padding: 15,
-    marginBottom: 5,
-    backgroundColor: '#eee',
-    borderRadius: 6,
-    flex: 1,
-  },
-  imagebox: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    borderRadius: 6,
-  },
-  textbox: {
-    flex: 2,
-    padding: 10,
-  },
-  listTitle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    paddingBottom: 15,
   },
 });
 
