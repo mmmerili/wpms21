051b32c186c2b9a62e02b05b94168f2efd50281c
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-08-24 13:48:23 +0300

    valmis-c

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,10 +1,36 @@
-// TODO: add necessary imports
-const apiUrl = 'http://media.mw.metropolia.fi/wbma/';
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
 const useMedia = () => {
-  // TODO: move mediaArray state here
-  // TODO: move loadMedia function here
-  // TODO: move useEffect here
-  return mediaArray;
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    // https://scriptverse.academy/tutorials/js-self-invoking-functions.html
+    (async () => {
+      setMediaArray(await loadMedia());
+    })();
+  }, []);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(baseUrl + 'media');
+      const mediaIlmanThumbnailia = await response.json();
+      const kaikkiTiedot = mediaIlmanThumbnailia.map(async (media) => {
+        return await loadSingleMedia(media.file_id);
+      });
+      return Promise.all(kaikkiTiedot);
+    } catch (e) {
+      console.log(e.message());
+    }
+  };
+
+  const loadSingleMedia = async (id) => {
+    const response = await fetch(baseUrl + 'media/' + id);
+    const tiedosto = await response.json();
+    return tiedosto;
+  };
+
+  return {mediaArray, loadMedia, loadSingleMedia};
 };
 
 export {useMedia};
