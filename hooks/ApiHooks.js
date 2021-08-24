8c918d08cd21df4bb41f46ab1fda64238821f468
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-08-24 13:16:45 +0300

    start-c

---
+++ hooks/ApiHooks.js
@@ -0,0 +1,10 @@
+// TODO: add necessary imports
+const apiUrl = 'http://media.mw.metropolia.fi/wbma/';
+const useMedia = () => {
+  // TODO: move mediaArray state here
+  // TODO: move loadMedia function here
+  // TODO: move useEffect here
+  return mediaArray;
+};
+
+export {useMedia};
