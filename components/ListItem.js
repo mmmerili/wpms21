ed30d28f11090a2cf567e9833ea86ecba096abb6
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-08-30 13:52:59 +0300

    navi valmis

--- components/ListItem.js
+++ components/ListItem.js
@@ -9,7 +9,7 @@ const ListItem = ({singleMedia, navigation}) => {
     <TouchableOpacity
       style={styles.row}
       onPress={() => {
-        navigation.navigate('Single');
+        navigation.navigate('Single', singleMedia);
       }}
     >
       <View style={styles.imagebox}>
--- views/Single.js
+++ views/Single.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Image} from 'react-native';
+import {uploadsUrl} from '../utils/variables';
 
-const Single = () => {
+const Single = ({route}) => {
+  const {params} = route;
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Single</Text>
+      <Text>{params.title}</Text>
+      <Image
+        style={{width: 200, height: 200}}
+        source={{uri: uploadsUrl + params.filename}}
+      />
+      <Text>{params.description}</Text>
+      <Text>{params.user_id}</Text>
+      <Text>{params.time_added}</Text>
+      <Text>{params.media_type}</Text>
     </SafeAreaView>
   );
 };
@@ -20,6 +30,8 @@ const styles = StyleSheet.create({
   },
 });
 
-Single.propTypes = {};
+Single.propTypes = {
+  route: PropTypes.object.isRequired,
+};
 
 export default Single;
