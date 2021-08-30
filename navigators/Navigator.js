3e3d193a9208a301dedf436611bc57361f85adad
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-08-30 13:16:48 +0300

    navi step 1

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import Single from '../views/Single';
 import Home from '../views/Home';
+import Profile from '../views/Profile';
+import Single from '../views/Single';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const TabScreen = () => {
+  return (
+    // TODO: move content of <NavigationContainer> here
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+const StackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Front" component={TabScreen} />
+      <Stack.Screen name="Single" component={Single} />
+    </Stack.Navigator>
+  );
+};
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Single" component={Single} />
-      </Tab.Navigator>
+      <StackScreen />
     </NavigationContainer>
   );
 };
